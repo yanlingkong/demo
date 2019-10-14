af32b4a8058cbcf910d78b81f90edce912036478
liuchao <liuchao@cashpie.cn>
2019-10-14 16:55:04 +0800

    代码提交

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * test测试类
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
+
